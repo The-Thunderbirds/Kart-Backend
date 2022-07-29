e6ec6dba92d8d799ca40236d127374d5b250564b
GaganAgarwal77 <Gagan.Agarwal@iiitb.ac.in>
2022-07-29 15:45:46 +0530

    Add buy item route and update constants

--- controllers/tz.mintkart.controller.js
+++ controllers/tz.mintkart.controller.js
@@ -1,10 +1,17 @@
 const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
-const { mint } = require('../services/tezos/index');
+const { buy } = require('../services/tezos/index');
 const { ADMIN_WALLET_PRIVATE_KEY, FA2_CONTRACT_ADDRESS, MINTKART_CONTRACT_ADDRESS } = require('../constants.js');
 
 exports._buy = asyncErrorHandler(async (req, res, next) => {
-    itemId = 'item-id-1';
-    buyer = 'tz1TuAWC3z9ZgEMgHWVUCPubgSMpSZEePK2k'
+    console.log(req.body);
+    const {serialNum, user_wallet_address} = req.body;
+    itemId = serialNum;
+    buyer = user_wallet_address;
+    console.log(itemId,buyer);
     const op = await buy(itemId, buyer, MINTKART_CONTRACT_ADDRESS, ADMIN_WALLET_PRIVATE_KEY);
     console.log(op);
+    res.status(200).json({
+        success: true,
+        op,
+    });
 });
--- routes/mintRoute.js
+++ routes/mintRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { _mint, _add_customer_service, _add_seller } = require('../controllers/tz.fa2.controller');
+const { _buy } = require("../controllers/tz.mintkart.controller");
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.route('/batch-mint').post(isAuthenticatedUser, authorizeRoles("seller"), _mint);
+router.route('/buy-item').post(isAuthenticatedUser, authorizeRoles("user"), _buy);
 
 module.exports = router;
 
