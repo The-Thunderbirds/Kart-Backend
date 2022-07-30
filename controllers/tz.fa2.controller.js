4fe311b49628d7520ac2def71d515e7317ae2b2c
GaganAgarwal77 <Gagan.Agarwal@iiitb.ac.in>
2022-07-30 21:54:41 +0530

    fix replace item function

--- controllers/tz.fa2.controller.js
+++ controllers/tz.fa2.controller.js
@@ -3,6 +3,7 @@ const { mint, init_replace_item, init_burn, add_seller, remove_seller, add_custo
 const { ADMIN_WALLET_PRIVATE_KEY, FA2_CONTRACT_ADDRESS, MINTKART_CONTRACT_ADDRESS } = require('../constants.js');
 const { encode, decode, generateId } = require('../utils/math');
 const Product = require('../models/productModel');
+const { v4: uuidv4 } = require('uuid');
 
 exports._mint = asyncErrorHandler(async (req, res, next) => {
     const {serialNums, password} = req.body;
@@ -79,21 +80,22 @@ exports._mint = asyncErrorHandler(async (req, res, next) => {
 });
 
 exports._init_replace_item = asyncErrorHandler(async (req, res, next) => {
-    const {serialNum, user_email} = req.body;
+    const {serialNum, password} = req.body;
+    const user = req.user;
+
     const products = await Product.find({serialNumber: serialNum});
     if (products.length === 0) {
         return next(new ErrorHandler("Product Not Found", 404));
     }
     const product = products[0];
-    const users = await User.find({email:user_email});
-    if (users.length === 0) {
-        return next(new ErrorHandler("User Not Found", 404));
-    }
-    const user = users[0];
+
+    user_private_key = decode(ADMIN_WALLET_PRIVATE_KEY, user.private_key, password);
+    private_key = user_private_key;// seller-private-key after decoding
+
     tokenId = product.nft_id;
     oldItemId = serialNum;
     newItemId = "sno-" + uuidv4();
-    private_key = user.private_key;
+
     const op = await init_replace_item(tokenId, oldItemId, newItemId, MINTKART_CONTRACT_ADDRESS, FA2_CONTRACT_ADDRESS, private_key);
     console.log(op);
     res.status(200).json({
