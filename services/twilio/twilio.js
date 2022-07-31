19dbbc9cb49aa8ba9bd13400ca2820f333d0ca43
GSri30 <sri_3007@hotmail.com>
2022-07-31 15:16:25 +0530

    fixed twilio message

--- services/twilio/twilio.js
+++ services/twilio/twilio.js
@@ -6,10 +6,10 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const buildMessage = (nft_id, serial_number, explorer) => {
-  return `Thank you for shopping with flipkart!
-          You have successfully claimed your warranty NFT. Please find the details below:
-          Item Serial Number: ${serial_number}
-          Warranty NFT ID: ${nft_id}
+  return `Thank you for shopping with flipkart!\\
+          You have successfully claimed your warranty NFT. Please find the details below:\\
+          Item Serial Number: ${serial_number}\\
+          Warranty NFT ID: ${nft_id}\\
           You can also verify the same at ${explorer}`;
 }
 
