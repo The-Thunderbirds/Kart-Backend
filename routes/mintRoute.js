21c90114cfd511e2833817b2d0a74fae861d3135
GaganAgarwal77 <Gagan.Agarwal@iiitb.ac.in>
2022-07-31 15:13:32 +0530

    Add burn route

--- routes/mintRoute.js
+++ routes/mintRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { _mint, _init_replace_item } = require('../controllers/tz.fa2.controller');
+const { _mint, _init_replace_item, _init_burn } = require('../controllers/tz.fa2.controller');
 const { _buy, fetchWarrantyDetails } = require("../controllers/tz.mintkart.controller");
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
@@ -9,6 +9,7 @@ router.route('/batch-mint').post(isAuthenticatedUser, authorizeRoles("seller"), 
 router.route('/buy-item').post(isAuthenticatedUser, authorizeRoles("user"), _buy);
 router.route('/fetch-warranty').post(isAuthenticatedUser, authorizeRoles("seller","customer-service"), fetchWarrantyDetails);
 router.route('/replace-item').post(isAuthenticatedUser, authorizeRoles("seller","customer-service"), _init_replace_item);
+router.route('/burn-nft/:id').get(isAuthenticatedUser, authorizeRoles("seller","customer-service"), _init_burn);
 
 module.exports = router;
 
